c1c8812335a40f2cd533902d89b67ae2c6d09b7d
Gayathri547 <89897862+Gayathri547@users.noreply.github.com>
2021-10-02 10:10:20 +0530

    Update 2_pwd.py

--- st_01-10-2021/2_pwd.py
+++ st_01-10-2021/2_pwd.py
@@ -2,7 +2,7 @@ c1,c2,c3 = 0,0,0
 x = input("enter your input:")
 if (len(x) >= 8):
     for i in x:
-        if ( i >='a' and i <= 'z') or (i >='A' and i <='Z'):
+        if ( i >='a' and i <= 'z') and (i >='A' and i <='Z'):
             c1 += 1       
         if i == '$' or i == '#' or i == '@' or i==' ' or i=='!' or i=='%' or i=='^' or i=='&' or i=='*' or i==',' or i=='.' or i=='/' or i=='<' or i=='>' or i=='|' or i=='{' or i=='}' or i=='[' or i==']' or i=='(' or i==')' or i==' ':
             c2 +=1
